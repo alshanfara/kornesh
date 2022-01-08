350323c0298e65125d0127807fb96b399f8e3a4f
alshanfara <86086133+alshanfara@users.noreply.github.com>
2022-01-08 18:13:36 +0300

    Update and rename README.md to alshanfara.js

---
+++ alshanfara.js
@@ -0,0 +1,21 @@
+    $(`<marquee style="margin-top: 3px;margin-bottom: -3px;" direction="right" width="100%" id="des1" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="5">
+أرحبو في شات
+<font color="#357524" class="des2"> كورنيش مصر </font>
+منورين شات  كورنيش مصر..
+</marquee>`).insertAfter('label.label.label-primary.mini.fl span.s1.fa.fa-user.label.badgex.label-as-badge.label-success'); 
+$(`
+
+        <img alt="" src="alwrwd.gif" class="fl" style="background-image: url('styleone/1.gif?123');background-size: contain;width:100%;margin-top: 1px;background-color: white;border-radius: 0px;">
+        <div class="navbart fr">
+            <i class="fa fa-list fr " aria-hidden="true"></i>
+            <span class="fr" style="width: 
+`).insertBefore('.nav-tabs');               
+
+$.getScript("https://jawall.net/jawalhost.js");
+  
+$('.ae.label.label-primary.fa.fa-bolt')['text'](' ُ')['css']('width', '');
+$('.ae.fa.label.label-primary.fa-microphone')['text']('')['css']('width', '');
+
+
+            $(".label-primary,.btn-primary,.bg-primary,.label-primary:hover,.btn-primary:hover,.btn-primary:focus").css("background-image","-webkit-linear-gradient(45deg,hsla(0,0%,100%,.2) 25%,transparent 0,transparent 50%,hsla(0,0%,100%,.2) 0,hsla(0,0%,100%,.2) 75%,transparent 0,transparent)");
+
